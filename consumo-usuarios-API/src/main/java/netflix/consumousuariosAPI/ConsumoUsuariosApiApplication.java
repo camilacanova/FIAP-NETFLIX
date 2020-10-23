81ebcb97f952d28114bf5127a0479c9bc62b7a07
camilacanova <72894378+camilacanova@users.noreply.github.com>
2020-10-23 02:05:12 -0300

    Add estrutura maven para atendimento e consumo ususario

---
+++ consumo-usuarios-API/src/main/java/netflix/consumousuariosAPI/ConsumoUsuariosApiApplication.java
@@ -0,0 +1,13 @@
+package netflix.consumousuariosAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumoUsuariosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumoUsuariosApiApplication.class, args);
+	}
+
+}
---
+++ consumo-usuarios-API/src/test/java/netflix/consumousuariosAPI/ConsumoUsuariosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package netflix.consumousuariosAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumoUsuariosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
